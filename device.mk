64ae307d20295359e1c039c376f066ecf1441bc7
rik0612c <qq872638399@gmail.com>
2023-03-02 16:29:58 +0800

    libra: remove offending entries build/make/core/main.mk:1308: warning:  device/xiaomi/libra/lineage_libra.mk includes non-existent modules in PRODUCT_PACKAGES Offending entries: cneapiclient com.quicinc.cne ipacm-diag libOmxVdecHevc libdivxdrmdecrypt qti-telephony-common services-ext build/make/core/main.mk:1308: error: Build failed. 16:26:44 ckati failed with: exit status 1

--- device.mk
+++ device.mk
@@ -71,10 +71,7 @@ PRODUCT_PACKAGES += \
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
-    cneapiclient \
-    com.quicinc.cne \
-    libcnefeatureconfig \
-    services-ext
+    libcnefeatureconfig
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
@@ -204,7 +201,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     libc2dcolorconvert \
-    libdivxdrmdecrypt \
     libdrmclearkeyplugin \
     libmm-omxcore \
     libOmxAacEnc \
@@ -213,7 +209,6 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libOmxVdec \
-    libOmxVdecHevc \
     libOmxVenc \
     libstagefrighthw
 
@@ -262,7 +257,6 @@ include $(LOCAL_PATH)/system_prop.mk
 PRODUCT_BOOT_JARS += telephony-ext
 
 PRODUCT_PACKAGES += \
-    qti-telephony-common \
     telephony-ext
 
 # TimeKeep
@@ -293,7 +287,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
-    ipacm-diag \
     libqsap_sdk \
     libwpa_client \
     readmac \
