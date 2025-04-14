1048a12661d1e731bf51cc980c996ffd0b255dbd
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-14 16:25:31 +0800

    libra: init: spoof selinux status
    this is futile since actual detectors can just test for rules.
    we keep this though in the meantime.

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,8 +1,9 @@
 #!/vendor/bin/sh
 
-# Enable ZRAM on devices with 2GB RAM
-if [ "$(awk '($1 == "MemTotal:") {print $2}' /proc/meminfo)" -lt 2097152 ]; then
-    echo "1G" >/sys/block/zram0/disksize
-    mkswap /dev/block/zram0
-    swapon /dev/block/zram0
+# selinux: Allow enforcing mode spoofing
+if [ "$(cat /sys/fs/selinux/enforce)" = 0 ]; then 
+	echo 2 > /sys/fs/selinux/enforce
 fi
+
+# EOF
+
