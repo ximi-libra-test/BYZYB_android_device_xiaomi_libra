0721b1e2907e624c8ed7e5379bc61866af657701
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-16 11:57:32 +0800

    linra: init: dont spoof selinux

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,9 +1,6 @@
 #!/vendor/bin/sh
 
-# selinux: Allow enforcing mode spoofing
-if [ "$(cat /sys/fs/selinux/enforce)" = 0 ]; then 
-	echo 2 > /sys/fs/selinux/enforce
-fi
+# do nothing for now
 
 # EOF
 
