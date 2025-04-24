2934e17ca0c35b5e25af7c5d8282e43dc1c0f768
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-24 22:29:10 +0800

    libra: init: remove overclock prop catcher
    does not really matter

--- system_prop.mk
+++ system_prop.mk
@@ -175,7 +175,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Special persist properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.cpu_overclock=false \
     persist.vendor.edge_touch_mode=false
 
 # Telephony
