c55abd9a7f46faeaf11b16322103500dc2d5b766
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-24 22:45:15 +0800

    libra: post_boot: re-tune mm / zram

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,6 +1,29 @@
 #!/vendor/bin/sh
+PATH=/vendor/bin:$PATH
 
-# do nothing for now
+# Memory Tuning
+# no need to reserve memory for these
+echo 0 > /proc/sys/vm/admin_reserve_kbytes
+echo 0 > /proc/sys/vm/user_reserve_kbytes
+
+# make sure zram is enabled
+# sometimes it fails especially on crdroid?
+if ! grep "zram" /proc/swaps > /dev/null 2>&1; then 
+	mkswap /dev/block/zram0 > /dev/null 2>&1
+	swapon /dev/block/zram0 > /dev/null 2>&1
+fi
+
+# tune if zram is up
+if grep "zram" /proc/swaps > /dev/null 2>&1; then 
+	# zram tweak
+	echo 200 > /proc/sys/vm/swappiness
+	echo 0 > /proc/sys/vm/page-cluster
+
+	# le9ec
+	echo 131072 > /proc/sys/vm/anon_min_kbytes
+	echo 0 > /proc/sys/vm/clean_min_kbytes
+	echo 131072 > /proc/sys/vm/clean_low_kbytes
+
+fi
 
 # EOF
-
