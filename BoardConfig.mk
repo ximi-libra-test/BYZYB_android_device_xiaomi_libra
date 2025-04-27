cc10e470b4c79e3c78bcb5b3fca0eb510cf6f28e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-28 00:34:54 +0800

    x

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,9 @@ TARGET_USES_OVERLAY := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := \
+     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+     vendor/lineage/config/device_framework_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_libra
