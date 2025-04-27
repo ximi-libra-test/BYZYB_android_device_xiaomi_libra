9bb0836a663bf0996f2d6e927c57f6928b8cc96b
rik0612c <qq872638399@gmail.com>
2025-04-27 23:46:30 +0800

    libra: Downgrade soundtrigger

--- device.mk
+++ device.mk
@@ -45,7 +45,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.0 \
+    android.hardware.soundtrigger@2.0-core \
+    android.hardware.soundtrigger@2.0-impl \
     audio.primary.msm8992 \
     audio.r_submix.default \
     audio.usb.default \
