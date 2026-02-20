fd3312f6c03f8cbe2ae345b31f934abd1eaa7f3c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-20 22:34:13 +0800

    libra: init: make sure zram works
    it sometimes fail

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,6 +1,13 @@
 #!/vendor/bin/sh
+PATH=/vendor/bin:$PATH
 
-# do nothing for now
+# Memory Tuning
+
+# make sure zram is enabled
+# sometimes it fails especially on crdroid?
+if ! grep "zram" /proc/swaps > /dev/null 2>&1; then 
+	mkswap /dev/block/zram0 > /dev/null 2>&1
+	swapon /dev/block/zram0 > /dev/null 2>&1
+fi
 
 # EOF
-
