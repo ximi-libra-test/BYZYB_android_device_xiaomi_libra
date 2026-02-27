c58dc0eca5c9f4038f010a753b2d2d649d9c61af
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-27 13:33:35 +0800

    libra: init: scripts: tune /proc/sys/vm tunables

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -10,4 +10,12 @@ if ! grep "zram" /proc/swaps > /dev/null 2>&1; then
 	swapon /dev/block/zram0 > /dev/null 2>&1
 fi
 
+# le9ec
+echo "65535" > /proc/sys/vm/clean_min_kbytes
+echo "131072" > /proc/sys/vm/anon_min_kbytes                                                                                                                                                       
+echo "262144" > /proc/sys/vm/clean_low_kbytes
+
+# raise swappiness after le9 is set
+echo "150" > /proc/sys/vm/swappiness
+
 # EOF
